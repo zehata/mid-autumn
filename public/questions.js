8524e56d4f20596086fad6defb3fab34a32c5311
zehata <zehata@gmail.com>
2016-09-16 08:06:26 +0800

    hello

---
+++ public/questions.js
@@ -0,0 +1,16 @@
+var question1 = JSON.stringify({answer:1, question:"传说中，在广寒宫中陪伴嫦娥的动物是？",option1:"兔子", option2:"猴子"});
+var question2 = JSON.stringify({answer:1, question:"有时落在山腰，有时挂在树梢， 有时象面圆镜，有时象把镰刀。（猜一天体）",option1:"月亮", option2:"太阳"});
+var question3 = JSON.stringify({answer:2, question:"为什么叫做“中秋节”",option1:"因为来自中国", option2:"因为在秋季的过了半才庆祝"});
+var question4 = JSON.stringify({answer:1, question:"在古代月圆和月缺一般形容什么？",option1:"悲欢离合", option2:"身体是否健康"});
+var question5 = JSON.stringify({answer:2, question:"传说中是谁偷了仙丹，飞上了月亮？",option1:"后羿", option2:"嫦娥"});
+var question6 = JSON.stringify({answer:1, question:"一面镜子亮晶晶，走遍天下照古今。",option1:"月亮", option2:"星星"});
+var question7 = JSON.stringify({answer:2, question:"一弯月照枝头亮，两颗星悬天下明。（猜一字）",option1:"明", option2:"秋"});
+var question8 = JSON.stringify({answer:2, question:"哪一位诗人有写关于中秋节的诗?",option1:"屈原", option2:"李白"});
+var question9 = JSON.stringify({answer:2, question:"按照传统，中秋节起源于？",option1:"宋朝", option2:"汉朝"});
+var question10 = JSON.stringify({answer:1, question:"纸包得住火。（猜一节日物品）",option1:"灯笼 ", option2:"烟花"});
+var question11 = JSON.stringify({answer:2, question:"皮也轻轻，骨也轻轻，心头轻轻，全身通明。（猜一物品）",option1:"路灯", option2:"灯笼"});
+var question12 = JSON.stringify({answer:1, question:"“月亮代表我的心” 这首歌是谁的作曲?",option1:"翁清渓", option2:"孙燕姿"});
+var question13 = JSON.stringify({answer:1, question:"“但愿人长久，千里共婵娟”出自哪位词人之手？",option1:"苏轼 ", option2:"辛弃疾"});
+var question14 = JSON.stringify({answer:1, question:"吃月饼的意义是什么?",option1:"团圆", option2:"幸福"});
+var question15 = JSON. stringify({answer:2, question:"能说会道",option1:"圆", option2:"团"});
+
